cbd85de780ffa79fa03bfb95dac2a3e5eb69ebfb
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-06 15:55:12 +0700

    check draw

--- src/main/java/com/mycompany/superxo/SuperXO.java
+++ src/main/java/com/mycompany/superxo/SuperXO.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.superxo;
 
 import java.util.Scanner;
@@ -11,7 +10,7 @@ import java.util.Scanner;
  * @author USER
  */
 public class SuperXO {
-    
+
     public String[][] list = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
     public String start;
     public String turn;
@@ -22,9 +21,38 @@ public class SuperXO {
 
     Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-         SuperXO project = new SuperXO();
-         System.out.println("com.mycompany.superxo.SuperXO.main()");
+    public boolean checkDraw() {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if (list[i][j].toLowerCase().equals("-")) {
+                    return false;
+                }
+
+            }
+
+        }
+        return true;
     }
-    
+
+    public void checkWin() {
+        if (isEnd == false) {
+            if (checkDraw()) {
+                isEnd = true;
+                System.out.println("+--------------------+");
+                System.out.println("|    !!! Draw !!!    |");
+                System.out.println("+--------------------+");
+                // show table
+                // ask to contonue
+//                if(continute = true) => reset
+//                else is end = true
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SuperXO project = new SuperXO();
+        System.out.println("com.mycompany.superxo.SuperXO.main()");
+    }
+
 }
